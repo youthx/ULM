6ded6662a40e835d463d7564e67015b26155bdfd
youthx <118225440+youthx@users.noreply.github.com>
2022-12-29 20:51:28 +0000

    types.h

---
+++ .ccls-cache/@home@runner@C-Graphics/ULM@headers@Types.h
@@ -0,0 +1,8 @@
+#ifndef TYPES_H
+#define TYPES_H
+#include <stdio.h>
+
+typedef float f32;
+typedef signed int i32;
+typedef unsigned int u32;
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "ULM/headers/Token.h"
+#include "ULM/headers/Types.h"
 
 int main(void) {
   return 0;
 }
-
